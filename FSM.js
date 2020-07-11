3495e60be99d6a7307897a967b31627c78b75b28
Retamogordo <yury.yukhananov@tutanota.com>
2020-07-11 17:44:00 -0400

    move RequestResponseFSM to a file apart

--- FSM.js
+++ FSM.js
@@ -1,184 +1,185 @@
+export { FSM, AsyncFSM }
 
-	function waitPromise(interval) { 
-		return new Promise( (resolve, _) => {
-					 setTimeout( () => { 
-					 	resolve( interval ); }, interval );
-					} );
-	}
+function waitPromise(interval) { 
+	return new Promise( (resolve, _) => {
+				 setTimeout( () => { 
+				 	resolve( interval ); }, interval );
+				} );
+}
 
-	function TransitionEntry(nextState, transitionCallback, transitionFailureState) {
-		this.nextState = nextState;
-		this.callback = transitionCallback;
-		this.failureState = transitionFailureState;
-	}
+function TransitionEntry(nextState, transitionCallback, transitionFailureState) {
+	this.nextState = nextState;
+	this.callback = transitionCallback;
+	this.failureState = transitionFailureState;
+}
 
-	function State(id, description, onCallback) {
-		this.id = id;
-		this.description = description;
-		this.on = onCallback;
-		
-		this.transitionMap = new Map();
-	}
+FSM.State = function(id, description, onCallback) {
+	this.id = id;
+	this.description = description;
+	this.on = onCallback;
+	
+	this.transitionMap = new Map();
+}
 
-	State.from = function(obj) {
+FSM.State.from = function(obj) {
 //		let state = new State(obj.id, obj.description, obj.onCallback);
-		let state = new State();
-		Object.assign(state, obj);
+	let state = new FSM.State();
+	Object.assign(state, obj);
 
-		return state;
-	}
+	return state;
+}
 
-	State.prototype.addTransition = function(signalID, transition) {
-		this.transitionMap.set(signalID, transition);
-		return transition.nextState;
-	};
+FSM.State.prototype.addTransition = function(signalID, transition) {
+	this.transitionMap.set(signalID, transition);
+	return transition.nextState;
+};
 
-	State.prototype.getTransition = function(sig) {
-		return this.transitionMap.get(sig);
-	}
+FSM.State.prototype.getTransition = function(sig) {
+	return this.transitionMap.get(sig);
+}
 
-	State.prototype.chain = function(signalId, toState, transitionCallback, failureState) {
-		this.addTransition(signalId, {nextState: toState, callback: transitionCallback, failureState});
-		return toState;
-	}
+FSM.State.prototype.chain = function(signalId, toState, transitionCallback, failureState) {
+	this.addTransition(signalId, {nextState: toState, callback: transitionCallback, failureState});
+	return toState;
+}
 
-	function Signal(id, transitionsCallbackArgs) {
-		this.id = id;
-		this.payload = transitionsCallbackArgs;
-	}
+FSM.Signal = function(id, transitionsCallbackArgs) {
+	this.id = id;
+	this.payload = transitionsCallbackArgs;
+}
 
 
-	FSM.changeState = function(fsm, sig, state) {
-		let result;
-		let transition = state.getTransition(sig.id);
+FSM.changeState = function(fsm, sig, state) {
+	let result;
+	let transition = state.getTransition(sig.id);
 
-		if ( transition ) {
-			try {
-				fsm.onLeave && fsm.onLeave(state);
-			}
-			// do not consume callback exceptions
-			finally	{
-				try	{	
-					result = (transition.callback && transition.callback(sig.payload)) || sig.payload;
-	//				console.log("result = ", result, "sig.payload: ", sig.payload)
-					state = transition.nextState;
-				}
-				catch (error) {
-					result = error;
-					console.log("FSM caught: ", error);
-
-					return transition.failureState || state;
-				}
-				(state.on && FSM.tryCallback(state.on, state)(state, result)) ||
-			 	(fsm.onSettle && FSM.tryCallback(fsm.onSettle, state)(state, result)); 
-			}
+	if ( transition ) {
+		try {
+			fsm.onLeave && fsm.onLeave(state);
 		}
-		return state;		
+		// do not consume callback exceptions
+		finally	{
+			try	{	
+				result = (transition.callback && transition.callback(sig.payload)) || sig.payload;
+//				console.log("result = ", result, "sig.payload: ", sig.payload)
+				state = transition.nextState;
+			}
+			catch (error) {
+				result = error;
+				console.log("FSM caught: ", error);
+
+				return transition.failureState || state;
+			}
+			(state.on && FSM.tryCallback(state.on, state)(state, result)) ||
+		 	(fsm.onSettle && FSM.tryCallback(fsm.onSettle, state)(state, result)); 
+		}
 	}
+	return state;		
+}
 
-	function FSM() {
-		this.idle = new State(-1, "Idle");
-		this.awaiting = new State(0, "Running, awaiting signals");
+function FSM() {
+	this.idle = new FSM.State(-1, "Idle");
+	this.awaiting = new FSM.State(0, "Running, awaiting signals");
 
-		this.idle.chain(FSM.runFSMSignal.id, this.awaiting);
-	
-		this.states = new Map();
-		this.states.set(this.idle.id, this.idle);
-		this.states.set(this.awaiting.id, this.awaiting);
+	this.idle.chain(FSM.runFSMSignal.id, this.awaiting);
 
+	this.states = new Map();
+	this.states.set(this.idle.id, this.idle);
+	this.states.set(this.awaiting.id, this.awaiting);
+
+	this.currState = this.idle;
+}
+
+FSM.tryCallback = (callback, fsmState) => {
+	return (...args) => {
+		try {
+			callback && callback(...args);
+		}
+		// do not consume callback exceptions
+		finally {
+			return fsmState;
+		}
+	}
+}
+
+FSM.prototype = (function () {
+	let protoObj = {};
+
+	FSM.runFSMSignal = {id: -1};
+
+	protoObj.constructor = FSM;
+
+	protoObj.init = function( objStates ) {
+		let state;
+
+		if (objStates instanceof Array) {
+			objStates.forEach( st => {
+				st = st instanceof FSM.State ? st : FSM.State.from(st);
+				this.states.set(st.id, st);
+
+				if (st.name) this[st.name] = st;
+			} )
+		}
+		else {
+			Object.keys(objStates).forEach( key => {
+				state = FSM.State.from(objStates[key]);
+				this.states.set(state.id, state);
+				this[key] = state;
+			});
+		}
+		this.stop();
+	}
+	protoObj.stop  = function() {
 		this.currState = this.idle;
+
+		FSM.tryCallback(this.onIdle, this.idle)(this.idle, "FSM stopped");
 	}
 
-	FSM.tryCallback = (callback, fsmState) => {
-		return (...args) => {
-			try {
-				callback && callback(...args);
-			}
-			// do not consume callback exceptions
-			finally {
-				return fsmState;
-			}
-		}
+	protoObj.run = function() {
+		this.inputSignal(FSM.runFSMSignal);
 	}
 
-	FSM.prototype = (function () {
-		let protoObj = {};
+	protoObj.inputSignal = function(sig) {
 
-		FSM.runFSMSignal = {id: -1};
+		this.currState = FSM.changeState(this, sig, this.currState);
+	}
+
+	protoObj.onSettle;
+
+	return protoObj;
+})();
+
+
+function AsyncFSM() {
+	FSM.call(this);
 	
-		protoObj.constructor = RequestResponseTimeoutPatternFSM;
+	this.promise = new Promise((resolve, _) => {
+						resolve(this.idle);
+					});
+}
+AsyncFSM.prototype = Object.create(FSM.prototype);
+AsyncFSM.prototype.constructor = AsyncFSM;
 
-		protoObj.init = function( objStates ) {
-			let state;
+AsyncFSM.prototype.stop  = function() {
+	this.promise = new Promise((resolve, _) => {
+						FSM.tryCallback(this.onIdle, this.idle)(this.idle, "FSM stopped");
+						
+						resolve(this.idle);
+					});
+}
 
-			if (objStates instanceof Array) {
-				objStates.forEach( st => {
-					st = st instanceof State ? st : State.from(st);
-					this.states.set(st.id, st);
+AsyncFSM.prototype.inputSignal = function(sig) {
+	this.promise = this.promise.then( state => {return FSM.changeState(this, sig, state)} );
+}
 
-					if (st.name) this[st.name] = st;
-				} )
-			}
-			else {
-				Object.keys(objStates).forEach( key => {
-					state = State.from(objStates[key]);
-					this.states.set(state.id, state);
-					this[key] = state;
-				});
-			}
-			this.stop();
-		}
-		protoObj.stop  = function() {
-			this.currState = this.idle;
-
-			FSM.tryCallback(this.onIdle, this.idle)(this.idle, "FSM stopped");
-		}
-
-		protoObj.run = function() {
-			this.inputSignal(FSM.runFSMSignal);
-		}
-
-		protoObj.inputSignal = function(sig) {
-
-			this.currState = FSM.changeState(this, sig, this.currState);
-		}
-
-		protoObj.onSettle;
-
-		return protoObj;
-	})();
-
-
-	function AsyncFSM() {
-		FSM.call(this);
-		
-		this.promise = new Promise((resolve, _) => {
-							resolve(this.idle);
-						});
-	}
-	AsyncFSM.prototype = Object.create(FSM.prototype);
-	AsyncFSM.prototype.constructor = AsyncFSM;
-
-	AsyncFSM.prototype.stop  = function() {
-		this.promise = new Promise((resolve, _) => {
-							FSM.tryCallback(this.onIdle, this.idle)(this.idle, "FSM stopped");
-							
-							resolve(this.idle);
-						});
-	}
-
-	AsyncFSM.prototype.inputSignal = function(sig) {
-		this.promise = this.promise.then( state => {return FSM.changeState(this, sig, state)} );
-	}
-
-	AsyncFSM.prototype.inputSignalDelayed = function(sig, delay) {
-		this.promise = this.promise.then( async state => {
-			await waitPromise(delay); 
-				
-			return state;
-		})
-		.then( state => {return FSM.changeState(this, sig, state)} );
-	}
+AsyncFSM.prototype.inputSignalDelayed = function(sig, delay) {
+	this.promise = this.promise.then( async state => {
+		await waitPromise(delay); 
+			
+		return state;
+	})
+	.then( state => {return FSM.changeState(this, sig, state)} );
+}
 //})()
 /*
 function InternalDataGeneratorFSM(dataGenerationCallback) {
@@ -209,292 +210,6 @@ InternalDataGeneratorFSM.prototype.run = function(initialInternalValue) {
 	AsyncFSM.prototype.run.call(this);
 }
 */
-function ShiftRegisterFSM(length, transitionCallback, loopBack) {
-	AsyncFSM.call(this);
-	
-	this.length = length;
-//	this.transitionDelay = transitionDelay;
-	this.transitionCallback = transitionCallback;
-	this.loopBack = loopBack;
-}
-
-ShiftRegisterFSM.prototype = Object.create(AsyncFSM.prototype);
-ShiftRegisterFSM.prototype.constructor = ShiftRegisterFSM;
-ShiftRegisterFSM.shiftSignalID = 100;
-//ShiftRegisterFSM.resetSignalID = 101;
-
-ShiftRegisterFSM.prototype.init = function () {
-	let state = this.awaiting;
-//	let firstState;
-	let id = 1;
-	let newState;
-	let length = this.length;
-	let transition;
-
-//	this.awaiting.on = () => { this.onReset && this.onReset(); }
-
-	let states = [];
-	while (length--) {
-		newState = state.chain( ShiftRegisterFSM.shiftSignalID, State.from({ id, name: "shiftState" + id}));
-		
-		transition = state.getTransition(ShiftRegisterFSM.shiftSignalID);
-//		transition.delay = this.transitionDelay;
-		transition.callback = this.transitionCallback;
-
-		state = newState;
-		
-		++id;
-
-		states.push(state);
-	}
-
-	if (this.loopBack) {
-		state.chain( ShiftRegisterFSM.shiftSignalID, this.awaiting);
-		transition = state.getTransition(ShiftRegisterFSM.shiftSignalID)
-//		transition.delay = this.transitionDelay;
-		transition.callback = this.transitionCallback;
-	} 
-	else {
-		state.chain( ShiftRegisterFSM.shiftSignalID, state);
-		state.on = () => { this.onLastSettle && this.onLastSettle(); }
-	}
-
-	FSM.prototype.init.call(this, states);
-}
-
-ShiftRegisterFSM.prototype.shift = function(transitionCallbackArgs, delay) {
-//	console.log("in shift, ", transitionCallbackArgs.id)
-
-	let sig = {id: ShiftRegisterFSM.shiftSignalID, payload: transitionCallbackArgs};
-	
-//	console.log("shift signal payload ", sig.payload)
-	delay ? this.inputSignalDelayed(sig, delay) : this.inputSignal(sig);
-//	this.inputSignal({id: ShiftRegisterFSM.shiftSignalID})
-}
-
-function RequestResponseTimeoutPatternFSM(retrials, retrialDelay) {
-	AsyncFSM.call(this);
-
-	this.retrialDelay = retrialDelay;
-
-	this.retryRegister = new ShiftRegisterFSM(retrials, this.genNextRequestTimestamp);
-
-	this.retryRegister.onIdle = () => { console.log("shift reg idle"); }
-
-	this.retryRegister.awaiting.on = () => { console.log("shift reg RJNNING"); }
-
-	this.retryRegister.onLastSettle = () => {
-		console.log("Last stage settled");
-
-		//	fsm1.inputSignal(sig);
-		this.inputSignal( { id: RequestResponseTimeoutPatternFSM.backToListeningSignalId,
-		 name: "Giving up => Back To  Waiting for Command" } );
-	}
-
-	this.retryRegister.onSettle = (state, timestamp) => {
-		console.log("Shift, timestamp: ", timestamp.id, "state:", state.name);
-
-		this.retryRegister.shift(timestamp, this.retrialDelay);
-//		this.currTimestamp = timestamp;	
-//		console.log(this.currTimestamp)
-
-		this.inputSignal({ id: RequestResponseTimeoutPatternFSM.timeoutSignalId,
-				payload: timestamp, name: "Response Timeout"});
-	}
-}
-
-RequestResponseTimeoutPatternFSM.startSyncSignalId = 99;
-RequestResponseTimeoutPatternFSM.sendRequestSignalId = 100;
-RequestResponseTimeoutPatternFSM.timeoutSignalId = 101;
-RequestResponseTimeoutPatternFSM.waitForResponseSignalId = 102;
-RequestResponseTimeoutPatternFSM.backToListeningSignalId = 103;
-RequestResponseTimeoutPatternFSM.responseReadySignalId = 104;
-RequestResponseTimeoutPatternFSM.dropResponseSignalId = 105;
-
-RequestResponseTimeoutPatternFSM.prototype = (function () {
-	let protoObj = Object.create(AsyncFSM.prototype);
-	protoObj.constructor = RequestResponseTimeoutPatternFSM;
-
-//	RequestResponseTimeoutPatternFSM.prototype = Object.create(AsyncFSM.prototype);
-//	RequestResponseTimeoutPatternFSM.prototype.constructor = RequestResponseTimeoutPatternFSM;
-
-	protoObj.onSendRequest;
-	//RequestResponseTimeoutPatternFSM.prototype.onRetry;
-	protoObj.onResponseReady;
-	protoObj.onResponseFailure;
-
-	protoObj.genNextRequestTimestamp = function(timestamp) {
-			let stamp = new Date().getTime();
-	//		console.log("genNextRequestTimestamp: ", timestampedRequest)
-			return timestamp ? {id: timestamp.id + 1, stamp} : {id: 1, stamp};
-	};
-
-	protoObj.init = function() {
-
-		const consumeResponse = response => {} 
-		const validateResponse = (response, timestamp) => { 
-			return (response.timestamp.id === timestamp.id && 
-				response.timestamp.stamp === timestamp.stamp); 
-		}
-
-		
-	//	const startingRetryRegisterCallback = (state, request) => { 
-		const startingRetryRegisterCallback = state => { 
-
-	//		request = this.genNextRequestTimestamp(request);
-			console.log("Starting Register settled, request: ");
-
-			this.retryRegister.run();
-
-			let timestamp = this.genNextRequestTimestamp();
-			this.retryRegister.shift(timestamp, this.retrialDelay);
-			
-			let sig = {id: RequestResponseTimeoutPatternFSM.sendRequestSignalId,
-	//				payload: request };
-					payload: timestamp };
-
-			this.inputSignal(sig);
-		}
-
-	//	const sendingRequestCallback = (state, req) => { 
-		const sendingRequestCallback = (state, timestamp) => { 
-		try {
-	//		console.log("Request sent settled, request: ", req);
-			console.log("Request sent settled, timestamp: ", timestamp);
-
-			this.request.timestamp = timestamp;
-
-		//	req.timestamp = this.currTimestamp;
-
-			this.inputSignal({id: RequestResponseTimeoutPatternFSM.waitForResponseSignalId, })
-	//			payload: req});
-	//			payload: timestamp});
-		
-			this.onSendRequest && this.onSendRequest(this.request);
-		}
-		catch (err) { console.log(err)}
-		}
-
-	//	const waitingForResponseCallback = (state, timestamp) => {
-		const waitingForResponseCallback = () => {
-	//		this.currentRequest = req;
-
-			console.log("Waiting for response ready settled");
-		}
-
-		const responseReadyCallback = (state, response) => { 
-			console.log("Response ready settled");
-
-			try {
-	//		if (validateResponse(response, this.currTimestamp)) {
-			if (validateResponse(response, this.request.timestamp)) {
-				console.log("Response valid !!!, timestamp: ", response.timestamp.id)
-
-				this.inputSignal({id: RequestResponseTimeoutPatternFSM.backToListeningSignalId});
-
-				this.onResponseReady && this.onResponseReady(response);
-			}
-			else {
-				this.inputSignal({id: RequestResponseTimeoutPatternFSM.dropResponseSignalId});
-				console.log("after drop")
-
-	//			this.onResponseFailure && this.onResponseFailure(response);
-			}
-			} catch(err) {console.log("err:", err)}
-		}
-
-		const backToListeningCallback = () => {
-			console.log("Back to listening settled, resetting shift reg.");
-
-			this.retryRegister.stop();
-
-		//	this.retryRegister.run();
-
-	//		this.currTimestamp = this.genNextTimestamp();
-		}
-
-		AsyncFSM.prototype.init.call(this, 
-			{	
-				startingRetryRegister: {id: 1, on: startingRetryRegisterCallback},
-				sendingRequest: {id: 2, on: sendingRequestCallback},
-				waitingForResponse: {id: 3, on: waitingForResponseCallback},
-				responseReady: {id: 4, on: responseReadyCallback},
-			} );
-
-		this.awaiting.on = backToListeningCallback;
-
-		this.awaiting
-			.chain(RequestResponseTimeoutPatternFSM.startSyncSignalId, this.startingRetryRegister)
-			.chain(RequestResponseTimeoutPatternFSM.sendRequestSignalId, this.sendingRequest)
-			.chain(RequestResponseTimeoutPatternFSM.waitForResponseSignalId, this.waitingForResponse)
-	//		.chain(RequestResponseTimeoutPatternFSM.timeoutSignalId, this.requestSent)	
-			.chain(RequestResponseTimeoutPatternFSM.timeoutSignalId, this.sendingRequest)	
-
-		this.waitingForResponse.chain(RequestResponseTimeoutPatternFSM.backToListeningSignalId, this.awaiting);
-
-		this.waitingForResponse.chain(RequestResponseTimeoutPatternFSM.responseReadySignalId, this.responseReady)
-			.chain(RequestResponseTimeoutPatternFSM.dropResponseSignalId, this.waitingForResponse);
-
-		this.responseReady.chain(RequestResponseTimeoutPatternFSM.backToListeningSignalId, this.awaiting);
-
-		this.retryRegister.init();
-	}
-
-	protoObj.send = function(request) {
-		this.request = request;
-
-		let sig = {id: RequestResponseTimeoutPatternFSM.startSyncSignalId,}
-	//				payload: request };
-
-		this.inputSignal(sig);
-	}
-
-	protoObj.receive = function(response) {
-		this.inputSignal({id: RequestResponseTimeoutPatternFSM.responseReadySignalId,
-			payload: response});
-	}
-
-    return protoObj;
-})();
-
-const receiveResponseDelayed = (response, delay) => {
-	setTimeout( 
-		() => { 
-			console.log("Got simulated response after ", delay, 
-				" ms, timestamp: ", response.timestamp.id, "message:", response.message);
-
-			fsm1.receive(response);
-			
-		}, delay );
-}
-
-let fsm1 = new RequestResponseTimeoutPatternFSM(4, 1000);
-
-fsm1.onSendRequest = (req) => { 
-	console.log("Sending Request by User");
-	receiveResponseDelayed({timestamp: req.timestamp, message: "QQQ"}, 2500); 
-}
-/*
-fsm1.onRetry = (req) => { 
-	console.log("Retrying Request");
-	receiveResponseDelayed({timestamp: req.timestamp, message: "ZZZ"}, 700);
- }
-*/
-fsm1.onResponseReady = (response) => {
-	console.log("Response: ", response.message, " consumed by User")
-
-	this.send({description: "Another request"});
-}
-
-fsm1.onResponseFailure = (response) => {
-	console.log("No Response available. over")
-}
-
-
-fsm1.init();
-fsm1.run();
-
-fsm1.send({description: "My request"});
 /*
 const receiveDelayed = ( delay) => {
 	setTimeout( 
